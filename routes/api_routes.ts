2d0e82fc4746ad2aa98dca5530c6e60804166214
dhineshneo <dhinesjkumar@neophrontech.com>
2026-06-05 11:09:51 +0530

    Latest commit

--- routes/api_routes.ts
+++ routes/api_routes.ts
@@ -51,4 +51,4 @@ export const API_ROUTES = {
 };
 
 export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-export const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_URL || "http://localhost:3003";
+export const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_URL || "http://demo.sevanta.in/api";
